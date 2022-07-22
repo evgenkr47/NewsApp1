7b212ed125a4096636e8025fa2b1aede12af008a
evgenkr47 <evgenkr47@gmail.com>
2022-07-22 15:27:14 +0700

    Lesson-3

---
+++ app/src/main/java/com/example/newsapp/utils/Const.kt
@@ -0,0 +1,9 @@
+package com.example.newsapp.utils
+
+class Const{
+
+    companion object{
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "271ac674efa84ae6ab04f92f251cadbd"
+    }
+}
